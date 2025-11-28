042b3a6b82494cd585586d5d1c45c7f5893c9fa5
Tanupriya Singh <tanu99007@gmail.com>
2025-11-28 09:56:47 +0530

    User.java

---
+++ Source package/com.online.ecommerce.dao/com.online.ecommerce.model/User.java
@@ -0,0 +1,36 @@
+package com.online.ecommerce.model;
+
+public abstract class User {
+    protected int id;
+    protected String name;
+    protected String email;
+    protected String password;
+    protected String role; // ADMIN / SELLER / BUYER
+
+    public User() {}
+
+    public User(int id, String name, String email, String password, String role) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.role = role;
+    }
+
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+
+    public String getPassword() { return password; }
+    public void setPassword(String password) { this.password = password; }
+
+    public String getRole() { return role; }
+    public void setRole(String role) { this.role = role; }
+
+    public abstract String getDashboardPath();
+}
