fbc99f4947b05a8c894a10dcbbcb91a3c0b32a5a
Tanupriya Singh <tanu99007@gmail.com>
2025-11-28 10:00:09 +0530

    UserNotFoundException.java

---
+++ Source package/com.online.ecommerce.dao/UserNotFoundException.java
@@ -0,0 +1,7 @@
+package com.online.ecommerce.exception;
+
+public class UserNotFoundException extends Exception {
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+}
