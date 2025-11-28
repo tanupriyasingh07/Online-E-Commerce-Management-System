af4be4f419ca108da3f1671076ac003546a904de
Tanupriya Singh <tanu99007@gmail.com>
2025-11-28 09:55:50 +0530

    Product.java

---
+++ Source package/com.online.ecommerce.dao/com.online.ecommerce.model/Product.java
@@ -0,0 +1,39 @@
+package com.online.ecommerce.model;
+
+public class Product {
+    private int id;
+    private String name;
+    private String description;
+    private double price;
+    private int sellerId;
+    private int stock;
+
+    public Product() {}
+
+    public Product(int id, String name, String description, double price, int stock, int sellerId) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.stock = stock;
+        this.sellerId = sellerId;
+    }
+
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public String getDescription() { return description; }
+    public void setDescription(String description) { this.description = description; }
+
+    public double getPrice() { return price; }
+    public void setPrice(double price) { this.price = price; }
+
+    public int getSellerId() { return sellerId; }
+    public void setSellerId(int sellerId) { this.sellerId = sellerId; }
+
+    public int getStock() { return stock; }
+    public void setStock(int stock) { this.stock = stock; }
+}
